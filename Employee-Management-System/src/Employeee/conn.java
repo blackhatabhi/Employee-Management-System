c51fea8fa8cbdecbea1423af658e5775b32454e3
blackhatabhi <abhinavpnp.kumar@gmail.com>
2021-05-06 14:02:13 +0530

    employ
    Signed-off-by: blackhatabhi <abhinavpnp.kumar@gmail.com>

---
+++ Employee-Management-System/src/Employeee/conn.java
@@ -0,0 +1,20 @@
+package Employeee;
+
+import java.sql.*;
+
+public class conn{
+    
+    public Connection c;
+    public Statement s;
+ 
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///project3","root","abhi6451");
+            s = c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+ 
